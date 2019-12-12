4daa38909442e80a7a534c95921ad743b59d4fa2
DaeunKang <jeangrey@xhiftcorp.com>
2019-12-12 16:54:48 +0900

    photo upload, link, display

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -79,6 +80,11 @@ class RoomAdmin(admin.ModelAdmin):
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    """ """
+    """ Photo Admin Definition """
 
-    pass
+    list_display = ("__str__", "get_thumnail")
+
+    def get_thumnail(self, obj):
+        return mark_safe(f"<img with ='50px' height='50px' src='{obj.file.url}' />")
+
+    get_thumnail.short_description = "Thumnail"
